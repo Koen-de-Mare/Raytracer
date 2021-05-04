62c5a73c2eda758ec8e88ed025f615185e0f993f
Koen de Mare <koenmare@gmail.com>
2021-05-04 22:29:42 +0200

    implemented importance sampling towards the light source

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
     println!("rendering...");
 
     let camera = Camera {
-	position: Vector{x: 0.001f32, y: -0.801f32, z: 1.0001f32},
+	position: Vector{x: 0.001f32, y: -0.801f32, z: 1.40001f32},
 	forward:  Vector{x: 0f32, y: 1f32, z: 0f32},
 	right:    Vector{x: 1f32, y: 0f32, z: 0f32},
 	up:       Vector{x: 0f32, y: 0f32, z: 1f32},
@@ -39,10 +39,10 @@ fn main() {
 	sphere: Sphere {
 	    position: Vector {
 		x: 0f32,
-		y: 0f32,
-		z: 3f32
+		y: 0.7f32,
+		z: 1.5f32
 	    },
-	    radius: 1.25f32,
+	    radius: 0.05f32,
 	    color: Color {
 		r: 1f32,
 		g: 1f32,
@@ -58,9 +58,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: -2f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.9f32,
-		b: 0.9f32,
+		r: 0.3f32,
+		g: 0.3f32,
+		b: 0.3f32,
 	    },
 	},
     });
@@ -70,9 +70,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: 2f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.9f32,
-		b: 0.9f32,
+		r: 0.3f32,
+		g: 0.3f32,
+		b: 0.3f32,
 	    },
 	},
     });
@@ -84,9 +84,9 @@ fn main() {
 	v2:   Vector{x: -2f32, y: 0f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.9f32,
-		b: 0.9f32,
+		r: 0.8f32,
+		g: 0.8f32,
+		b: 0.8f32,
 	    },
 	},
     });
@@ -96,9 +96,9 @@ fn main() {
 	v2:   Vector{x: 2f32, y: 0f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.9f32,
-		b: 0.9f32,
+		r: 0.8f32,
+		g: 0.8f32,
+		b: 0.8f32,
 	    },
 	},
     });
@@ -111,9 +111,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: -2f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.9f32,
-		b: 0.2f32,
+		r: 0.8f32,
+		g: 0.8f32,
+		b: 0.1f32,
 	    },
 	},
     });
@@ -123,9 +123,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: 2f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.9f32,
-		b: 0.2f32,
+		r: 0.8f32,
+		g: 0.8f32,
+		b: 0.1f32,
 	    },
 	},
     });
@@ -137,9 +137,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: 0f32, z: -2f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.2f32,
-		g: 0.9f32,
-		b: 0.9f32,
+		r: 0.1f32,
+		g: 0.8f32,
+		b: 0.8f32,
 	    },
 	},
     });
@@ -149,9 +149,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: 0f32, z: 2f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.2f32,
-		g: 0.9f32,
-		b: 0.9f32,
+		r: 0.1f32,
+		g: 0.8f32,
+		b: 0.8f32,
 	    },
 	},
     });
@@ -163,9 +163,9 @@ fn main() {
 	v2:   Vector{x: -2f32, y: 0f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.2f32,
-		b: 0.9f32,
+		r: 0.8f32,
+		g: 0.1f32,
+		b: 0.8f32,
 	    },
 	},
     });
@@ -175,9 +175,9 @@ fn main() {
 	v2:   Vector{x: 2f32, y: 0f32, z: 0f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.9f32,
-		g: 0.2f32,
-		b: 0.9f32,
+		r: 0.8f32,
+		g: 0.1f32,
+		b: 0.8f32,
 	    },
 	},
     });
@@ -189,9 +189,9 @@ fn main() {
 	v2:   Vector{x: 0f32, y: 0f32, z: 2f32},
 	material: Material {
 	    diffuse_color: Color {
-		r: 0.8f32,
-		g: 0.8f32,
-		b: 0.8f32,
+		r: 0.5f32,
+		g: 0.5f32,
+		b: 0.5f32,
 	    },
 	},
     });
@@ -199,6 +199,44 @@ fn main() {
 	base: Vector{x: -1f32, y: -1f32, z: 2f32},
 	v1:   Vector{x: 2f32, y: 0f32, z: 0f32},
 	v2:   Vector{x: 0f32, y: 0f32, z: -2f32},
+	material: Material {
+	    diffuse_color: Color {
+		r: 0.5f32,
+		g: 0.5f32,
+		b: 0.5f32,
+	    },
+	},
+    });
+
+    //occluder
+    scene.triangles.push(Triangle {
+	base: Vector{x: 0f32, y: 0.4f32, z: 1.2f32},
+	v1:   Vector{x: 0.5f32, y: 1f32, z: 0f32},
+	v2:   Vector{x: -1f32, y: 1f32, z: 0.4f32},
+	material: Material {
+	    diffuse_color: Color {
+		r: 0.9f32,
+		g: 0.9f32,
+		b: 0.9f32,
+	    },
+	},
+    });
+    scene.triangles.push(Triangle {
+	base: Vector{x: 0f32, y: 0.4f32, z: 1.2f32},
+	v1:   Vector{x: 0f32, y: 1f32, z: -0.5f32},
+	v2:   Vector{x: 0.5f32, y: 1f32, z: 0f32},
+	material: Material {
+	    diffuse_color: Color {
+		r: 0.9f32,
+		g: 0.9f32,
+		b: 0.9f32,
+	    },
+	},
+    });
+    scene.triangles.push(Triangle {
+	base: Vector{x: 0f32, y: 0.4f32, z: 1.2f32},
+	v1:   Vector{x: -1f32, y: 1f32, z: 0.4f32},
+	v2:   Vector{x: 0f32, y: 1f32, z: -0.5f32},
 	material: Material {
 	    diffuse_color: Color {
 		r: 0.9f32,
@@ -224,7 +262,7 @@ fn main() {
 
 	    let ray = camera.shoot_ray(x, y);
 
-	    let num_samples = 200;
+	    let num_samples = 25;
 	    let mut accumulator = BLACK;
 	    for _ in 0 .. num_samples {
 		accumulator = accumulator + scene.trace_ray(ray, 5);
@@ -234,7 +272,14 @@ fn main() {
 	}
     }
 
-    rendering.apply_gamma(0.75f32);
+    println!("{:#?}", rendering.pixels[width * height - 300]);
+    println!("{:#?}", rendering.pixels[width * height - 400]);
+    println!("{:#?}", rendering.pixels[width * height - 500]);
+    println!("{:#?}", rendering.pixels[width * height - 600]);
+    println!("{:#?}", rendering.pixels[width * height - 700]);
+    
+    
+    rendering.apply_gamma(0.2f32);
     
     println!("saving...");
     
--- src/scene.rs
+++ src/scene.rs
@@ -10,11 +10,12 @@ use std::f32::consts::PI;
 enum SamplingMethod {
     Uniform,
     NaiveImportanceSampling, // cosine weighed, not aware of the position of the light source
-    //AwareImportanceSampling, // exploits knowledge of the position of the light source
+    AwareImportanceSampling, // exploits knowledge of the position of the light source
 }
 
-const SAMPLING_METHOD: SamplingMethod = SamplingMethod::Uniform;
+//const SAMPLING_METHOD: SamplingMethod = SamplingMethod::Uniform;
 //const SAMPLING_METHOD: SamplingMethod = SamplingMethod::NaiveImportanceSampling;
+const SAMPLING_METHOD: SamplingMethod = SamplingMethod::AwareImportanceSampling;
 
 #[derive(Clone, Debug)]
 pub struct Scene {
@@ -47,6 +48,10 @@ impl Scene {
     // convention for direction_in to be OUT OF the surface
     // i.e. both in the direction of ray tracing, and opposite to the direction of the light
     fn light_out(&self, surface_element: SurfaceElement, _direction_out: Vector, recurse: i32) -> Color {
+	assert!(surface_element.normal.is_normal());
+	assert!(_direction_out.is_normal());
+	assert!(recurse >= 0);
+	
 	if recurse == 0 {
 	    return BLACK;
 	}
@@ -71,7 +76,7 @@ impl Scene {
 		flux_in * surface_element.material.diffuse_color * (theta.cos() * 2f32)
 	    },
 	    SamplingMethod::NaiveImportanceSampling => {
-		let p: f32 = rand::random();
+		let p: f32 = rand::random::<f32>();
 		let theta: f32 = (1f32 - 2f32 * p).acos() / 2f32;
 
 		let omega: f32 = 2f32 * PI * rand::random::<f32>();
@@ -89,7 +94,98 @@ impl Scene {
 
 		flux_in * surface_element.material.diffuse_color
 	    },
-	    //AwareImportanceSampling => {},
+	    SamplingMethod::AwareImportanceSampling => {
+		// calculate the disc related to what part of the light source is above the horizon
+		// in case the light souce is entirely below the horizon, defaults to a zero-width cone allong the normal
+		let (direction_disc, disc_angle) = {
+		    let direction_sphere = (self.sphere.position - surface_element.position).normalised();
+		    let cos_theta_sphere = dot(surface_element.normal, direction_sphere);
+		    let theta_sphere = cos_theta_sphere.acos();
+		    
+		    let distance_sphere = (self.sphere.position - surface_element.position).norm();
+
+		    // angle between the lines from the surface element to the center of the sphere and to the edge of the sphere
+		    let apparent_angle = (self.sphere.radius / distance_sphere).atan();
+
+		    let theta_min = theta_sphere - apparent_angle;
+
+		    if theta_min > PI * 0.5f32 {
+			// sphere is not visible at all
+			(surface_element.normal, 0f32)
+		    } else {
+			let theta_max = (theta_sphere + apparent_angle).min(PI * 0.5f32);
+			let theta_disc = (theta_max + theta_min) * 0.5f32;
+			let disc_angle   = (theta_max - theta_min) * 0.5f32;
+
+			let direction_sphere_in_plane = (direction_sphere - surface_element.normal * cos_theta_sphere).normalised();
+
+			let direction_disc = surface_element.normal * theta_disc.cos() + direction_sphere_in_plane * theta_disc.sin();
+
+			(direction_disc, disc_angle)
+		    }
+		};
+		
+		let brightness_disc = (self.sphere.color.r + self.sphere.color.g + self.sphere.color.b) / 3f32;
+		let area_disc = 2f32 * PI * (1f32 - disc_angle.cos());
+		
+		let brightness_ambient = 0.001f32; //TEMP <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		
+		//make sure alpha behaves appropriately when area_disc goes to zero
+		let alpha =
+		    (brightness_disc - brightness_ambient) / (
+			brightness_disc - brightness_ambient * (1f32 - PI / (area_disc * dot(direction_disc, surface_element.normal)))
+		    ).max(0f32);
+		assert!(alpha <= 1f32);
+		if disc_angle == 0f32 {
+		    assert!(alpha == 0f32);
+		}
+		
+		let p1 = rand::random::<f32>();
+
+		let direction_in = if p1 < alpha {
+		    // sample on the disc pointing towards the light source
+		    assert!(disc_angle > 0f32);
+
+		    let p2 = rand::random::<f32>();
+		    let cos_theta = 1f32 - p1 * (1f32 - disc_angle.cos());
+		    let theta = cos_theta.acos();
+
+		    let p2 = rand::random::<f32>();
+		    let omega = 2f32 * PI * p2;
+
+		    let (v1, v2) = direction_disc.make_orthogonal_frame();
+		    direction_disc * cos_theta + (v1 * omega.cos() + v2 * omega.sin()) * theta.sin()
+		} else {
+		    // sample cosine-weighed
+		    let p: f32 = rand::random::<f32>();
+		    let theta: f32 = (1f32 - 2f32 * p).acos() / 2f32;
+
+		    let omega: f32 = 2f32 * PI * rand::random::<f32>();
+
+		    let (v1, v2) = surface_element.normal.make_orthogonal_frame();
+		    let direction_in = surface_element.normal * theta.cos() + (v1 * omega.cos() + v2 * omega.sin()) * theta.sin();
+
+		    direction_in
+		};
+		assert!(direction_in.is_normal());
+		
+		let towards_disc = dot(direction_in, direction_disc) > disc_angle.cos();
+
+		let denominator = if towards_disc {
+		    1f32 + alpha * (PI / (area_disc * dot(direction_in, surface_element.normal)) - 1f32)
+		} else {
+		    1f32 - alpha
+		};
+		
+		let ray = Ray {
+		    origin: surface_element.position,
+		    direction: direction_in,
+		};
+
+		let flux_in = self.trace_ray(ray, recurse - 1);
+
+		flux_in * surface_element.material.diffuse_color * (1f32 / denominator)
+	    },
 	}
     }
     
